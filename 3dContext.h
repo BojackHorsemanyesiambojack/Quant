9a21f45f324f817c233e737106ad582b3ea0b06f
Johandry Luna <147267829+BojackHorsemanyesiambojack@users.noreply.github.com>
2025-02-01 10:01:05 -0600

    Primitive Sphere Class

--- 3dContext.h
+++ 3dContext.h
@@ -1,30 +1,35 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #define NUM_VAOS 1
 
-namespace TDC{
-	float cameraX, cameraY, cameraZ;
-	GLuint mvLoc, projLoc, vLoc, tfLoc;
-	glm::mat4 pMat, vMat, mMat, mvMat, tMat, rMat;
-	int windowHeigth, windowWidth;
-	float aspect;
+namespace TDC {
+    float cameraX = 0.0f, cameraY = 0.0f, cameraZ = 5.0f;
+    GLuint mvLoc, projLoc, vLoc, tfLoc;
+    glm::mat4 pMat, vMat, mMat, mvMat, tMat, rMat;
+    int windowHeight, windowWidth;
+    float aspect;
 
-	int tdcEvents(GLuint *VAO, GLFWwindow* window, GLuint program) {
-		getActualFrameSize(window);
-		updateUniformVariables(program);
-	}
+    void getActualFrameSize(GLFWwindow* window) {
+        glfwGetFramebufferSize(window, &windowWidth, &windowHeight);
+        aspect = static_cast<float>(windowWidth) / static_cast<float>(windowHeight);
 
-	void updateUniformVariables(GLuint program) {
-		mvLoc = glGetUniformLocation(program, "mv_matrix");
-		projLoc = glGetUniformLocation(program, "proj_matrix");
-		vLoc = glGetUniformLocation(program, "v_matrix");
-		tfLoc = glGetUniformLocation(program, "tf");
-	}
+        pMat = glm::perspective(glm::radians(45.0f), aspect, 0.1f, 100.0f);
+    }
 
-	void getActualFrameSize(GLFWwindow* window) {
-		glfwGetFramebufferSize(window, &windowWidth, &windowHeigth);
-		aspect = float(windowWidth) / float(windowHeigth);
-	}
+    void updateUniformVariables(GLuint program) {
+        mvLoc = glGetUniformLocation(program, "mv_matrix");
+        projLoc = glGetUniformLocation(program, "proj_matrix");
+        vLoc = glGetUniformLocation(program, "v_matrix");
+        tfLoc = glGetUniformLocation(program, "tf");
+
+        glUniformMatrix4fv(projLoc, 1, GL_FALSE, &pMat[0][0]);
+    }
+
+    void tdcEvents(GLuint* VAO, GLFWwindow* window, GLuint program) {
+        getActualFrameSize(window);
+        updateUniformVariables(program);
+    }
 }
